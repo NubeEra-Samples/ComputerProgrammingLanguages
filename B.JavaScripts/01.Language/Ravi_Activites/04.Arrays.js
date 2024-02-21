ac4ff17a64ef6450d8216c4e744d4293dc0e608c
ravib7 <ravib7@gmail.com>
2024-02-21 13:48:03 +0530

    added questins in arrays.js

--- B.JavaScripts/01.Language/Ravi_Activites/04.Arrays.js
+++ B.JavaScripts/01.Language/Ravi_Activites/04.Arrays.js
@@ -1,7 +1,7 @@
 
 // Array Initialization: Create an array named fruits containing the names of three fruits of your choice.
 
-// let fruits = ["Mango, Banana, Apple"]
+// let fruits = ["Mango", "Banana", "Apple"];
 // console.log(fruits)
 
 // _____________________________________________________________________________________________________________________
@@ -14,4 +14,79 @@
 // _________________________________________________________________________________________________________________________
 
 // Array Modification: Replace the first fruit in the fruits array with "Banana".
+
+// let fruits = ["Mango", "Banana", "Apple"];
+// fruits [0] = "Banana"
+// console.log(fruits) 
+
+// _____________________________________________________________________________________________________________________________
+
+// Array Length: Print the number of elements in the fruits array.
+
+// let fruits = ["Mango", "Banana", "Apple"];
+// console.log(fruits.length)
+
+// ______________________________________________________________________________________________________________________________
+
+// Array Iteration: Use a loop to print each fruit in the fruits array on a new line.
+
+// let fruits = ["Mango", "Banana", "Apple"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]); 
+// }
+
+// __________________________________________________________________________________________________________
+
+// Array Sorting: Sort the fruits array in alphabetical order.
+
+// let fruits = ["Mango", "Banana", "Apple"];
+// console.log(fruits.sort())
+
+// ____________________________________________________________________________________________________________________
+
+// Array Addition: Add two more fruits ("Grapes" and "Watermelon") to the fruits array.
+
+// let fruits = ["Mango", "Banana", "Apple"];
+// fruits.push("Grapes", "Watermelon")
+// console.log(fruits)
+
+// ___________________________________________________________________________________________________________________________
+
+// Array Removal: Remove the third fruit from the fruits array.
+
+// let fruits = ['Mango', 'Banana', 'Apple', 'Grapes', 'Watermelon'];
+// delete fruits[3]
+// console.log(fruits)
+
+// ____________________________________________________________________________________________________________________
+
+// Array Searching: Check if "Apple" exists in the fruits array. Print "Yes" if it does, otherwise print "No".
+
+// let fruits = ["Mango", "Banana", "Apple"];
+
+// if (fruits.includes("Apple"))
+// {
+//     console.log("Yes Apple is avalible")
+// }
+// else
+// {
+//     console.log("No Apple is not avalible")
+// }
+
+// ____________________________________________________________________________________________________
+
+// Array Concatenation: Create another array named moreFruits containing at least two additional
+// fruits. Then, combine the fruits array with the moreFruits array to create a new array named
+// allFruits
+
+// let fruits = ["Mango", "Banana", "Apple"];
+// let moreFruits = ["Grapes", "Watermelon"]
+
+// let allFruits = fruits.concat(moreFruits)
+
+// console.log(allFruits)
+
+
+
 
