1110a3f0a42921258064855725255b480add58df
ravib7 <ravib7@gmail.com>
2024-03-27 11:46:37 +0530

    added file

---
+++ B.JavaScripts/01.Language/Ravi_Activites/01.Language/09.for-loop.js
@@ -0,0 +1,84 @@
+// Question 1
+
+// for (let i=1; i <= 10; i++)
+// {
+//     console.log(i)
+// }
+
+// Question 2
+
+// for (let i=2; i <= 30;  i += 2)
+// {
+//     console.log(i)
+// }
+
+// Question 3
+
+// for (let i=10; i >= 1; i--)
+// {
+//     console.log(i)
+// }
+
+
+// Question 4
+
+// let sum = 0;
+// for (let i=0; i <= 100; i++)
+// {
+//     sum += i
+// }
+// console.log(sum)
+
+
+// Question 5
+
+// let tab = 5;
+// for (let i=1; i <= 10; i++) 
+// {
+//     console.log(tab, "*",  i, "=", tab * i)
+// }
+
+// Question 6
+
+// Method-1
+// let array = ["Ravi", "Majed", "Taher", "Vishal", "Durgesh", "Sham"]
+// for (a in array)
+// {
+//     console.log(array[a])
+// }
+
+// Method-2
+// let array = ["Ravi", "Majed", "Taher", "Vishal", "Durgesh"];
+// for (let name of array) {
+//     console.log(name);
+// }
+
+// Question 7
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let i = 1; i < num.length; i++)
+// {
+//     if (num[i] % 2 !== 0)
+//     console.log(num[i])
+// }
+
+// Question 8
+
+// let str = "WelCome all of you"
+// for (let i = 0; i < str.length; i++)
+// {
+//     console.log(str[i])
+// }
+
+// Question 9
+
+// const numbers = [10, 20, 30, 40, 50];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// const average = sum / numbers.length;
+// console.log(average);
+
+
+
